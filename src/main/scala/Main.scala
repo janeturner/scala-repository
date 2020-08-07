e455926334df83c9fa37ee323863ccc36cf77e0e
janeturner <jane.turner@digital.hmrc.gov.uk>
2020-08-07 16:19:40 +0100

    Create read and write hello functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,29 @@
-object Main extends App {
-    def sayHello() = {
-        "Hello World"
+import java.io._
+import scala.io.Source
+
+object Main {
+
+    def main(args: Array[String]): Unit = {
+
+        println(sayHello)
+
+        writeHello()
+
+        readHello()
     }
 
+    val file = "hello.txt"
+    val sayHello = "Hello World"
+
+    def writeHello() = {
+        val pw = new PrintWriter(new File(file))
+        pw.write(sayHello)
+        pw.close
+    }
+
+    def readHello() = {
+        for (line <- Source.fromFile(file).getLines) {
+            println(line)
+        }
+    }
 }
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -3,7 +3,7 @@ import org.scalatest.FunSuite
 class MainTest extends FunSuite {
 
   test("should say Hello World") {
-      assert(Main.sayHello() == "Hello World")
+      assert(Main.sayHello == "Hello World")
 
     }
 
