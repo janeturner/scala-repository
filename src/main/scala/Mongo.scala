b1efcd0b2d3f45e902a3724da333a8b8aaad16f7
muhammadibraaheem <46523489+muhammadibraaheem@users.noreply.github.com>
2020-09-07 16:21:01 +0100

    add CRUD functionality

--- src/main/scala/Mongo.scala
+++ src/main/scala/Mongo.scala
@@ -1,12 +1,21 @@
 import org.mongodb.scala._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Projections._
+import org.mongodb.scala.model.Sorts._
+
 
 object Mongo {
 
-  def helloMongo() = {
-    val mongoCollection = MongoClient().getDatabase("TestDatabase").getCollection("NewCollection")
+  val mongoCollection = MongoClient().getDatabase("TestDatabase").getCollection("NewCollection")
+
+  def writeMongo() = {
 
     val mongoDocument = Document("""{"key":"value"}""")
-
     mongoCollection.insertOne(mongoDocument).toFutureOption()
+  }
+
+  def readMongo() = {
+    val list = mongoCollection.find()
+    println(list)
   }
 }
--- src/test/scala/MongoTest.scala
+++ src/test/scala/MongoTest.scala
@@ -3,10 +3,14 @@ import org.scalatest.FunSuite
 class MongoTest extends FunSuite {
 
   test("should say Hello Mongo") {
-     Mongo.helloMongo()
+     Mongo.writeMongo()
       assert(true)
     }
 
+  test("should find all data in coleection") {
+    Mongo.readMongo()
+    assert(true)
+  }
 
 
 }
